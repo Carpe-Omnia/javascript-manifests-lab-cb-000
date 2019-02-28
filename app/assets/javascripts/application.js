f26a8b80c052e5417632eeafc900717b1fc2f506
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-28 19:29:12 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require wrong_place_wrong_time
-//= require hello_world.js
-//= require learn.js
-//= require main.js
+//= require alert_maker
+//= require main
